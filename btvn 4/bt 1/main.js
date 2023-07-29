296a23d065b48d8552eb6906aa21f64fd5469013
grasspath <87555168+dragon19227@users.noreply.github.com>
2023-07-29 21:47:23 +0700

    jkjkkjjkjkjk
    uuiuyujdhgkfjdhdsjksdkjfdshdfkjhdf

---
+++ btvn 4/bt 1/main.js
@@ -0,0 +1,6 @@
+// fav lang is js
+localStorage.setItem('MyfavouriteLanguage','javascript')
+//fav lang is ts 
+localStorage.setItem('MyfavouriteLanguage','typescript')
+// both language sucks
+localStorage.setItem('comment','these thing sucks')
